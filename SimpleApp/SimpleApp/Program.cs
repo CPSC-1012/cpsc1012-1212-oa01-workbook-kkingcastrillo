8590c5ea73b931d32c1b46469bff7677674b3a81
kkingcastrillo <kkingcastrillo1@nait.ca>
2022-01-06 16:51:21 -0700

    Write/WriteLine

--- SimpleApp/SimpleApp/Program.cs
+++ SimpleApp/SimpleApp/Program.cs
@@ -7,6 +7,9 @@ namespace SimpleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, can you C#?");
+            Console.WriteLine("without your eye glases");
+            Console.Write("Can you see the difference?");
+            Console.Write(" between Write and WriteLine");
         }
     }
 }
