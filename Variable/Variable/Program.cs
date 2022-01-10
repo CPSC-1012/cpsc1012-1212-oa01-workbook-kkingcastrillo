6a5e5a5e0cca2975322e3d72469f73fff4befe29
kkingcastrillo <kkingcastrillo1@nait.ca>
2022-01-10 15:41:34 -0700

    Variable

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue;
+
+            intValue = 5;
+
+            Console.Write("The value is ");
+            Console.WriteLine(intValue);
+        }
+    }
+}
