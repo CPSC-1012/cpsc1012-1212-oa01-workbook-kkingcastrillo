110715f61672e6d00c7e2a135566b327e3fb91b3
kkingcastrillo <kkingcastrillo1@nait.ca>
2022-01-10 16:23:09 -0700

    Sale - End of January, 10th

---
+++ Sale/Sale/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price, tax, total;
+
+            price = 29.75;
+            tax = 1.76;
+            total = 31.51;
+
+            Console.WriteLine($"The price of the item is {price:C}");
+            Console.WriteLine($"The tax is {tax:C}");
+            Console.WriteLine($"The total is {total:C}");
+            Console.ReadLine();
+        }
+    }
+}
