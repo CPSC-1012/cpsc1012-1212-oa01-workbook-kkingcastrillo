27a2a7d571410f34b5aa7b76d9ae725a0aaab12e
kkingcastrillo <kkingcastrillo1@nait.ca>
2022-01-10 15:30:00 -0700

    Tabs

---
+++ Tabs/Tabs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("These are are our top sellers: \n");
+            Console.Write("\tItem 1\n\tItem 2\n\tItem 3\n");
+            Console.ReadLine();
+        }
+    }
+}
