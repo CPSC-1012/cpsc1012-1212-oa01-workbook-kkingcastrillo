7a597a076290de8a94fb318c9ae4660449168b68
kkingcastrillo <kkingcastrillo1@nait.ca>
2022-01-17 16:47:03 -0700

    2022-01-17

---
+++ AdditionQuiz/AdditionQuiz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AdditionQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int minNumber = 1;
+            const int maxNumber = 9;
+            Random keygen = new Random();
+
+            int number1 = keygen.Next(minNumber, maxNumber + 1);
+            int number2 = keygen.Next(minNumber, maxNumber + 1);
+
+            int correctAnswer = number1 + number2;
+
+            Console.WriteLine($"What is {number1} + {number2} = ?");
+            int userAnswer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{number1} + {number2} = {userAnswer} is {userAnswer == correctAnswer}");
+        }
+    }
+}
---
+++ SubstractionQuiz/SubstractionQuiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SubstractionQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random keygen = new Random();
+            int number1 = keygen.Next(1, 10);
+            int number2 = keygen.Next(1, 10);
+
+            if (number1 < number2)
+            {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            Console.WriteLine($"What is {number1} - {number2} = ?");
+            int answer = int.Parse(Console.ReadLine());
+
+            if (number1 - number2 == answer)
+            {
+                Console.WriteLine("You are correct.");
+            }
+            else
+            {
+                Console.WriteLine("Your answer is wrong.");
+                Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+            }
+        }
+    }
+}
